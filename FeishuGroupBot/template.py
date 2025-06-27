38318681415e441688aa8ae01e606fb3e915b0b8
Moxin <1044631097@qq.com>
2025-06-27 17:50:43 +0800

    feat(template): ✨ 增加飞书富文本消息模板

--- FeishuGroupBot/template.py
+++ FeishuGroupBot/template.py
@@ -1,6 +1,8 @@
 import requests
 import json
 
+
+# 发送飞书卡片消息
 def send_feishu_card_message(webhook_url, title, message):
     """
     发送卡片消息到飞书机器人 webhook。
@@ -62,6 +64,39 @@ def send_feishu_card_message(webhook_url, title, message):
     return response
 
 
+# 发送飞书富文本消息
+def send_feishu_rich_text_message(webhook_url, title, content):
+    """
+    发送富文本消息到飞书机器人 webhook。
+
+    :param webhook_url: 飞书机器人的 webhook 地址
+    :param title: 发送的标题
+    :param content: 要发送的消息 列表 list 参考：https://open.feishu.cn/document/client-docs/bot-v3/add-custom-bot#f62e72d5
+    :return: 响应对象
+    """
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    payload = {
+        "msg_type": "post",
+        "content": {
+            "post": {
+                "zh_cn": {
+                    "title": title,
+                    "content": [
+                        content
+                    ]
+                }
+            }
+        }
+    }
+
+    response = requests.post(webhook_url, headers=headers, data=json.dumps(payload))
+    return response
+
+
+# 发送飞书文本消息
 def send_feishu_text_message(webhook_url, message):
     """
     发送文本消息到飞书机器人 webhook。
